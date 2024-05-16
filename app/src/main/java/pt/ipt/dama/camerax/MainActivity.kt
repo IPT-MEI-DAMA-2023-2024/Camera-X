c581774f467de68a62db8c6a14a5a853ba6edef8
jcnpereira <casimiro@ipt.pt>
2024-05-16 20:47:16 +0100

    ask for permissions

--- app/src/main/java/pt/ipt/dama/camerax/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/camerax/MainActivity.kt
@@ -1,15 +1,19 @@
 package pt.ipt.dama.camerax
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.camera.camera2.internal.annotation.CameraExecutor
 import androidx.camera.core.ImageCapture
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.content.ContextCompat
 import pt.ipt.dama.camerax.databinding.ActivityMainBinding
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,26 +38,83 @@ class MainActivity : AppCompatActivity() {
         // app behavior
         // **********************************************
 
-    //ask for permissions
-    if(allPermissionsGranted()) {
-        startCamera()
-    }else{
-        requestPermissions()
-    }
+        //ask for permissions
+        if (allPermissionsGranted()) {
+            //  startCamera()
+        } else {
+            requestPermissions()
+        }
 
         // Button behavior
         viewBinding.imageCaptureButton.setOnClickListener {
-            takePhoto()
+            //  takePhoto()
         }
 
         // configure access to camera
         // use of Singleton pattern
-        cameraExecutor=Executors.newSingleThreadExecutor()
+        cameraExecutor = Executors.newSingleThreadExecutor()
 
     }
 
+    /**
+     * define if all permissions has been granted
+     */
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it
+        ) == PackageManager.PERMISSION_GRANTED
+    }
 
 
+    /**
+     * ask for permissions
+     */
+    private fun requestPermissions() {
+        activityResultLauncher.launch(REQUIRED_PERMISSIONS)
+    }
 
+
+    /**
+     * if it is necessary to ask for permissions,
+     * this will evaluate the answers provided by user
+     * and start the camera, or inform user that it can not use the camera
+     */
+    private val activityResultLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        )
+        { permissions ->
+
+            // Handle Permission granted/rejected
+            var permissionGranted = true
+            permissions.entries.forEach {
+                // test for all types of permissions
+                if (it.key in REQUIRED_PERMISSIONS && !it.value)
+                    permissionGranted = false
+            }
+            if (!permissionGranted) {
+                Toast.makeText(
+                    baseContext,
+                    "Permission request denied",
+                    Toast.LENGTH_SHORT
+                ).show()
+            } else {
+                //   startCamera()
+            }
+        }
+
+    companion object {
+        private const val TAG = "CameraXApp"
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                // import android.Manifest
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
+    }
 
 }
