86c2c562d5dce5068b82fbf69fe08bd30acd0049
jcnpereira <casimiro@ipt.pt>
2024-05-16 19:49:28 +0100

    skeleton of our code

--- app/src/main/java/pt/ipt/dama/camerax/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama/camerax/MainActivity.kt
@@ -3,18 +3,57 @@ package pt.ipt.dama.camerax
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.camera2.internal.annotation.CameraExecutor
+import androidx.camera.core.ImageCapture
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import pt.ipt.dama.camerax.databinding.ActivityMainBinding
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var cameraExecutor: ExecutorService
+    private var imageCapture: ImageCapture? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+        // using 'viewBinding'
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        // **********************************************
+        // app behavior
+        // **********************************************
+
+    //ask for permissions
+    if(allPermissionsGranted()) {
+        startCamera()
+    }else{
+        requestPermissions()
     }
+
+        // Button behavior
+        viewBinding.imageCaptureButton.setOnClickListener {
+            takePhoto()
+        }
+
+        // configure access to camera
+        // use of Singleton pattern
+        cameraExecutor=Executors.newSingleThreadExecutor()
+
+    }
+
+
+
+
+
 }
